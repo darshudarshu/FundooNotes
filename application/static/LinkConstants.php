82ada20ad3c766bc4b7bf96de1e3f672df18e2d8
Your Name <you@example.com>
2018-12-06 10:56:42 +0530

    FundooNotes

--- application/static/LinkConstants.php
+++ application/static/LinkConstants.php
@@ -3,8 +3,8 @@ class LinkConstants
 {
  
     public $verificationLinkMasssage = " hello click this link to verify your email click here ";
-    public $verificationLink         = "http://localhost:4200/verify?token=";
-    public $resetLinkMasssage        = "hello Click this link to recover your password click here";
-    public $resetLink                = "http://localhost:4200/reset?token=";
+    public $verificationLink         = " http://localhost:4200/verify?token= ";
+    public $resetLinkMasssage        = " hello Click this link to recover your password click here ";
+    public $resetLink                = " http://localhost:4200/reset?token= ";
 
 }
