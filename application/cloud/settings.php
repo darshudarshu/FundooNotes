de0e62040acce476f573d4bbcf22f18205473e64
Your Name <you@example.com>
2018-12-07 14:30:49 +0530

    cloudinary config

--- application/cloud/settings.php
+++ application/cloud/settings.php
@@ -1,6 +1,6 @@
 <?php
 \Cloudinary::config(array(
     "cloud_name" => "aaaaaaaa",
-    "api_key"    => "3333333333333333",
-    "api_secret" => "56ergdse56edhgr78ryrd57",
+    "api_key"    => "33333333333333333",
+    "api_secret" => "56ergdsef6edhgr78ryrd57",
 ));
